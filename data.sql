6c29a427c56ebd249dd31d37c3a15ad0627340d9
Rishtha03 <151524588+Rishtha03@users.noreply.github.com>
2024-12-19 20:23:20 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO question (question_text, options, correct_answer) VALUES
+('What is the capital of France?', '["Paris", "Berlin", "Madrid", "Rome"]', 'Paris'),
+('What is 2 + 2?', '["3", "4", "5", "6"]', '4');
